556bb4b6b9e5910928158feeff325acf97ac6e71
akriel <stirbu.alexandru.net@outlook.com>
2022-08-04 10:30:58 +0000

    added config

--- train/train.py
+++ train/train.py
@@ -7,9 +7,9 @@ from data_loader.meld_data_module import MeldDataModule
 from models.multi_input_perceiver import MultiInputPerceiverParallel
 from models.multi_input_perceiver_pl import MultiInputPerceiverPL
 
-
 NUM_WORDS = 6336
 MAX_LEN = 88
+
 
 def model_train(
         mip_config: MultiInputPerceiverParallel.Config,
@@ -37,9 +37,9 @@ def model_train(
     pl_module = MultiInputPerceiverPL(
         model_config=mip_config,
         lr=lr,
-        num_words = NUM_WORDS,
-        text_dim = text_dim,
-        audio_dim = audio_dim,
+        num_words=NUM_WORDS,
+        text_dim=text_dim,
+        audio_dim=audio_dim,
     )
 
     datamodule = MeldDataModule(
@@ -54,20 +54,40 @@ def model_train(
 
 if __name__ == '__main__':
     mip_config = MultiInputPerceiverParallel.Config(
-        input_channels=[2048, 256],
+        input_channels=[1024, 64],
         input_axis=[1, 1],
 
-        num_classes=7
+        latent_dim=128,
+        num_latents=128,
+
+        depth=2,
+
+        cross_heads=8,
+        cross_dim_head=64,
+
+        self_per_cross_attn=8,
+        latent_heads=8,
+        latent_dim_head=64,
+
+        attn_dropout=0.2,
+        ff_dropout=0.2,
+        merged_dropout=0.2,
+
+        max_freq=10,
+        num_freq_bands=6,
+        fourier_encode_data=True,
+
+        num_classes=7,
+        final_classifier_head=True,
     )
 
     model_train(
         mip_config=mip_config,
         lr=1e-3,
         batch_size=4,
-        num_workers=0,
+        num_workers=4,
         category='emotion',
-        max_len=100,
-        text_dim=2048,
-        audio_dim=256,
+        max_len=MAX_LEN,
+        text_dim=1024,
+        audio_dim=64,
     )
-
